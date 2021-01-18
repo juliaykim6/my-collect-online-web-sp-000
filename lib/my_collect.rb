64d9eac45fcd426ebdd6c8023f176d08bafc3a61
Julia Kim <juliaykim6@gmail.com>
2021-01-18 02:25:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
 
 
+end
+
+
+
+
+
+#Build a method that yields members of a collection to a block.
+#Control the return value of a method that uses yield
+# such that it returns a new collection.
